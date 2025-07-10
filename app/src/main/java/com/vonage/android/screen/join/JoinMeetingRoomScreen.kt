158cf67fb882e7db443d2b2c6dc94b4d6f589c6d
Manuel Vera <manuel.veranieto@vonage.com>
2025-07-10 10:04:02 +0200

    VIDSOL-93 update emulator configuration

--- app/src/main/java/com/vonage/android/screen/join/JoinMeetingRoomScreen.kt
+++ app/src/main/java/com/vonage/android/screen/join/JoinMeetingRoomScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.vonage.android.R
@@ -281,6 +282,7 @@ fun RoomInput(
     }
 }
 
+@PreviewScreenSizes
 @Preview
 @Composable
 internal fun PreviewJoinRoomScreen() {
