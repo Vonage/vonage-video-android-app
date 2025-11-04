a1c04ac03a39cef2223de7a0d4a22f544f2be866
Manuel Vera <manuel.veranieto@vonage.com>
2025-11-04 11:42:32 +0100

    VIDSOL-204 add in app updates

--- app/src/main/java/com/vonage/android/MainActivity.kt
+++ app/src/main/java/com/vonage/android/MainActivity.kt
@@ -8,15 +8,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavDeepLinkRequest
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.rememberNavController
 import com.vonage.android.compose.theme.VonageVideoTheme
 import com.vonage.android.navigation.AppNavHost
+import com.vonage.android.util.InAppUpdates
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -27,6 +32,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         enableEdgeToEdge()
+
+        // Register InApp updates
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+                InAppUpdates(this@MainActivity).register()
+            }
+        }
+
         setContent {
             VonageVideoTheme {
                 InterceptorAppNavHost(flow)
