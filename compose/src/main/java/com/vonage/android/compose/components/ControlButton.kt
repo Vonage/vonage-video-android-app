ee3f363dc78b95ac9799c1bb7d1589d453728f69
Manuel Vera <manuel.veranieto@vonage.com>
2025-11-20 17:53:47 +0100

    VIDSOL-295 cleanup

--- compose/src/main/java/com/vonage/android/compose/components/ControlButton.kt
+++ compose/src/main/java/com/vonage/android/compose/components/ControlButton.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,7 +22,7 @@ fun ControlButton(
     isActive: Boolean,
     modifier: Modifier = Modifier,
 ) {
-    val iconColor = remember(isActive) { if (isActive) Color.White else Color.Gray }
+    val iconColor = if (isActive) Color.White else Color.Gray
 
     Box(
         modifier = modifier
