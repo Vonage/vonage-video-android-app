2afd494d001355e5058e50a12bbece9a8a54ef7d
Manuel Vera <manuel.veranieto@vonage.com>
2025-12-04 11:08:08 +0100

    VIDSOL-356 visual fixes and adaptive bottom bar

---
+++ app/src/main/java/com/vonage/android/screen/room/components/bottombar/layoutSelectorAction.kt
@@ -0,0 +1,31 @@
+package com.vonage.android.screen.room.components.bottombar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.vonage.android.R
+import com.vonage.android.compose.vivid.icons.VividIcons
+import com.vonage.android.compose.vivid.icons.solid.Apps
+import com.vonage.android.compose.vivid.icons.solid.Layout2
+import com.vonage.android.screen.room.CallLayoutType
+import com.vonage.android.screen.room.MeetingRoomActions
+
+@Composable
+fun layoutSelectorAction(
+    layoutType: CallLayoutType,
+    roomActions: MeetingRoomActions,
+): BottomBarAction =
+    when (layoutType) {
+        CallLayoutType.SPEAKER_LAYOUT -> BottomBarAction(
+            type = BottomBarActionType.CHANGE_LAYOUT,
+            icon = VividIcons.Solid.Layout2,
+            label = stringResource(R.string.change_layout),
+            onClick = { roomActions.onChangeLayout(CallLayoutType.GRID) },
+        )
+
+        else -> BottomBarAction(
+            type = BottomBarActionType.CHANGE_LAYOUT,
+            icon = VividIcons.Solid.Apps,
+            label = stringResource(R.string.change_layout),
+            onClick = { roomActions.onChangeLayout(CallLayoutType.SPEAKER_LAYOUT) },
+        )
+    }
