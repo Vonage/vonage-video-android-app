89733186d15ff03f1ad42cfbe63a2008bb949bdb
Manuel Vera <manuel.veranieto@vonage.com>
2026-03-25 09:13:02 +0100

    VIDSOL-545: Improved captions UI (#107)

--- vonage-feature-captions/src/disabled/java/com/vonage/android/captions/ui/CaptionsOverlay.kt
+++ vonage-feature-captions/src/disabled/java/com/vonage/android/captions/ui/CaptionsOverlay.kt
@@ -2,12 +2,13 @@ package com.vonage.android.captions.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.vonage.android.kotlin.model.CallFacade
+import com.vonage.android.kotlin.model.CaptionLine
+import kotlinx.collections.immutable.ImmutableList
 
 @Suppress("UnusedParameter", "EmptyFunctionBlock")
 @Composable
 fun CaptionsOverlay(
-    call: CallFacade,
+    captionLines: ImmutableList<CaptionLine>,
     modifier: Modifier = Modifier,
 ) {
 
