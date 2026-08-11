bd0e8adf2a1443fe6c1ff2d1b79b9f884200f2c0
Manuel Vera <manuel.veranieto@vonage.com>
2026-08-11 11:38:08 +0200

    fix: fixes after merge

--- vonage-video-core/src/test/java/com/vonage/android/kotlin/CallTest.kt
+++ vonage-video-core/src/test/java/com/vonage/android/kotlin/CallTest.kt
@@ -23,7 +23,6 @@ import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
