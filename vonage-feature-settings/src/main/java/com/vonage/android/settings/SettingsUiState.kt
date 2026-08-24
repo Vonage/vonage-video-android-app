3ac560a28c48fe10022ad24b2ab3c02a8398809b
Manuel Vera <manuel.veranieto@vonage.com>
2026-08-24 17:03:47 +0200

    shrink: unify videoBitrateConfig default to single source of truth
    The default VideoBitrateConfig value was copy-pasted in three files:
    CallSettingsHolder, SettingsUiState, and PersistedCallSettings.
    Make CallSettingsHolder and SettingsUiState both derive the default
    from PersistedCallSettings() so it only lives in one place.

--- vonage-feature-settings/src/main/java/com/vonage/android/settings/SettingsUiState.kt
+++ vonage-feature-settings/src/main/java/com/vonage/android/settings/SettingsUiState.kt
@@ -6,8 +6,8 @@ import com.vonage.android.kotlin.model.CaptureFrameRate
 import com.vonage.android.kotlin.model.CaptureResolution
 import com.vonage.android.kotlin.model.DegradationPreference
 import com.vonage.android.kotlin.model.VideoBitrateConfig
-import com.vonage.android.kotlin.model.VideoBitratePreset
 import com.vonage.android.kotlin.model.VideoCodec
+import com.vonage.android.settings.PersistedCallSettings
 import com.vonage.android.shared.ViewState
 
 @Stable
@@ -19,10 +19,7 @@ data class SettingsUiState(
     val opusDtxEnabled: Boolean = true,
     val publisherAudioFallbackEnabled: Boolean = true,
     val subscriberAudioFallbackEnabled: Boolean = true,
-    val videoBitrateConfig: VideoBitrateConfig = VideoBitrateConfig(
-        preset = VideoBitratePreset.DEFAULT,
-        maxBitrate = VideoBitratePreset.DEFAULT.defaultMaxBitrate,
-    ),
+    val videoBitrateConfig: VideoBitrateConfig = PersistedCallSettings().videoBitrateConfig,
     val degradationPreference: DegradationPreference = DegradationPreference.NOT_SET,
     val captureFrameRate: CaptureFrameRate = CaptureFrameRate.FPS_15,
     val captureResolution: CaptureResolution? = null,
